0f109636a6757315bf5417d3fa53749fe468f346
nurulamin092 <mnurulamin092@gmail.com>
2026-01-30 16:56:35 +0600

    prisma connect and oth file

---
+++ src/lib/prisma.ts
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+import { PrismaClient } from "../../generated/prisma/client";
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | unknown;
+};
+const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export { prisma };
